82cd8f388e34b013f005a9a36209806a9c956f01
Tobit Prince <68125101+tobitprince@users.noreply.github.com>
2025-02-23 01:34:03 +0300

    Fix formatting in new_gemini.py by removing unnecessary newline

--- new_gemini.py
+++ new_gemini.py
@@ -7,6 +7,5 @@ API_TOKEN = config.GEMINI_API_KEY
 client = genai.Client(api_key= API_TOKEN)
 response = client.models.generate_content(
     model="gemini-2.0-flash", contents="Explain how AI works"
-    
 )
 print(response.text)
